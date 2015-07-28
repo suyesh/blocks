da7b4254252a550135d3725e469e009a98044b7d
Suyesh Bhandari <suyeshb@gmail.com>
2015-07-28 06:03:00 -0400

    deal

--- deal.rb
+++ deal.rb
@@ -1,7 +1,19 @@
 def deal
   faces = ["Jack", "Queen", "King", "Ace"]
   suits = ["Hearts", "Diamonds", "Spades", "Clubs"]
+  if block_given?
   random_face = faces.sample
   random_suit = suits.sample
   #call the block here
+  score = yield(random_face, random_suit)
+  puts "You scored a #{score}"
+else
+  puts "No Deal!"
 end
+
+end
+
+deal #do |face, suit|
+  #puts "Dealt a #{face} of #{suit}"
+  #face.length + suit.length
+#end
